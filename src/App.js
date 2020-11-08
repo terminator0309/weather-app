e0558acb8c7f5f5adf115a583fe87bb7141b53d0
Sandeep Mandal <sandeepmandal415@gmail.com>
2020-11-08 20:20:22 +0530

    Starting all over again

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Main from "./components/Main";
 
 function App() {
-  return <Main />;
+  return <></>;
 }
 
 export default App;
--- src/components/WeekPredictions.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function WeekPredictions() {
-  return <div style={{ height: "65vh", background: "black" }}></div>;
-}
